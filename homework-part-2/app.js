4977b68187f8f9e079ad397d87fcdc2e79593654
Daniel Becker <danielbecker@Daniels-MacBook-Pro.local>
2018-09-11 20:06:10 -0600

    Homweork-part-2

---
+++ homework-part-2/app.js
@@ -0,0 +1,50 @@
+// Part 1
+
+// 1. inteprolation is using a variable to insert data into a console.log().
+// concatenation is creating a string of information in a console.log() using +.
+// 2. DRY stands for Don't Repeat Yourself. We should use it to create more readable
+// and less complicated code. Functions are a good example of DRY code.
+// 3. Declairing a variable is naming it and assigning value gives the variable
+// something to look for. Defining a variable is asking what value a given
+// variable has.
+// 4. You should use const when you do not want to redefine a variable, and let
+// when you do want to redefine a variable. 
+// 5. A parent directory is a folder that contains other folders and files.
+// 6. Tab completion is how you auto-complete directory and file names in 
+// the command line. It is awesome because it makes navigating much faster.
+
+
+// Part 2
+
+// 1. const a < b;
+// 2. const c < d;
+// 3. const 'Name' === 'Name'; 
+// 4. const a < b < c;
+// 5. const a === a < d;
+// 6. const e = 'Kevin';
+// 7. 48 = '48';
+
+
+// Part 3
+
+// 1. Yes this is an infinate loop, because there is no condition being met
+// to end the loop.
+//
+// 2. 
+//
+// 3. Count up letters starting with A and ending at T.
+
+
+// Part 4
+
+// 1. A while loop is used when you need to repeat something until it becomes true
+// and a for loop is used to iterate a given number of items. 
+
+
+for(let i = 0; i < 1000; i++); {
+    console.log(i);
+}
+
+for(let i = 1; i <= 10; i++); {
+    console.log(`The value of i is; ${i}`)
+}
